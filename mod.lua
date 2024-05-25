4edc6159d654bdfe891a427812a2afb058074450
Florian Thiel <flth@flth.de>
2024-05-25 20:17:36 +0200

    Initial

---
+++ mod.lua
@@ -0,0 +1,28 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "germany", "bahn", "n-Wagen", "UIC-X", "coach", "silberling" },
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+	
+	runFn = function (settings)
+	
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,153 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "n-Wagen",
+		["mod_desc"] = "Als n-Wagen wird eine Gattung von Personenwagen der Deutschen Bundesbahn beziehungsweise der Deutschen Bahn AG bezeichnet. Umgangssprachlich ist die Wagengattung als Silberling bekannt geworden. Diese Bezeichnung verdanken die Nahverkehrswagen der Farbe des ursprünglich unlackierten Wagenkastens aus blankem rostfreiem Stahl mit aufgebürstetem Pfauenaugenmuster. Die Wagen wurden zwischen 1958 und 1980 in rund 5000 Exemplaren gebaut und sind teilweise bis heute bei DB Regio im Einsatz. ",
+
+        ["menu_nwagen"] = "n-Wagen",
+        ["nwagen_kl2_vorserie_name"] = "B4nb (Vorserie)",
+		["nwagen_kl2_vorserie_desc"] = "Vorserienexemplar der n-Wagen, Farbgebung: chromoxidgrün.",
+
+        ["nwagen_kl2_silber1_name"] = "B4nb",
+		["nwagen_kl2_silber1_desc"] = "n-Wagen (\"Silberling\") als 2. Klasse-Wagen und schwarzem Längsträger.",
+        ["nwagen_kl1_silber1_name"] = "AB4nb",
+		["nwagen_kl1_silber1_desc"] = "n-Wagen (\"Silberling\") als 1./2. Klasse-Wagen und schwarzem Längsträger.",
+
+        ["nwagen_kl2_silber2_name"] = "Bnb 720",
+		["nwagen_kl2_silber2_desc"] = "n-Wagen (\"Silberling\") als 2. Klasse-Wagen und schwarzem Längsträger.",
+        ["nwagen_kl1_silber2_name"] = "ABnb 703",
+		["nwagen_kl1_silber2_desc"] = "n-Wagen (\"Silberling\") als 1./2. Klasse-Wagen und schwarzem Längsträger.",
+
+        ["nwagen_kl2_silber3_name"] = "Bnb 720",
+		["nwagen_kl2_silber3_desc"] = "n-Wagen (\"Silberling\") als 2. Klasse-Wagen und ozeanblauem Längsträger.",
+        ["nwagen_kl1_silber3_name"] = "ABnb 703",
+		["nwagen_kl1_silber3_desc"] = "n-Wagen (\"Silberling\") als 1./2. Klasse-Wagen und ozeanblauem Längsträger.",
+
+        ["nwagen_hase_silber1_forward_name"] = "BD4nf (Steuerabteil voraus)",
+        ["nwagen_hase_silber1_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger mit dem Steuerabteil nach vorne und Fahrer.",
+        ["nwagen_hase_silber1_backward_name"] = "BD4nf (Steuerabteil hinten)",
+        ["nwagen_hase_silber1_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger als Nachläufer mit Steuerabteil nach hinten.",
+        ["nwagen_hase_silber1_ohne_forward_name"] = "BD4nf (Steuerabteil voraus)",
+        ["nwagen_hase_silber1_ohne_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger mit dem Steuerabteil nach vorne und Fahrer. Front ohne Gummiwulst und Übergang.",
+        ["nwagen_hase_silber1_ohne_backward_name"] = "BD4nf (Steuerabteil hinten)",
+        ["nwagen_hase_silber1_ohne_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger als Nachläufer mit Steuerabteil nach hinten. Front ohne Gummiwulst und Übergang.",
+
+        ["nwagen_hase_silber2_forward_name"] = "BDnf 738 (Steuerabteil voraus)",
+        ["nwagen_hase_silber2_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger mit dem Steuerabteil nach vorne und Fahrer.",
+        ["nwagen_hase_silber2_backward_name"] = "BDnf 738 (Steuerabteil hinten)",
+        ["nwagen_hase_silber2_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger als Nachläufer mit Steuerabteil nach hinten.",
+        ["nwagen_hase_silber2_ohne_forward_name"] = "BDnf 738 (Steuerabteil voraus)",
+        ["nwagen_hase_silber2_ohne_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger mit dem Steuerabteil nach vorne und Fahrer. Front ohne Gummiwulst und Übergang.",
+        ["nwagen_hase_silber2_ohne_backward_name"] = "BDnf 738 (Steuerabteil hinten)",
+        ["nwagen_hase_silber2_ohne_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger als Nachläufer mit Steuerabteil nach hinten. Front ohne Gummiwulst und Übergang.",
+
+        ["nwagen_hase_silber3_ohne_forward_name"] = "BDnf 738 (Steuerabteil voraus)",
+        ["nwagen_hase_silber3_ohne_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit ozeanblauem Längsträger mit dem Steuerabteil nach vorne und Fahrer. Front ohne Gummiwulst und Übergang.",
+        ["nwagen_hase_silber3_ohne_backward_name"] = "BDnf 738 (Steuerabteil hinten)",
+        ["nwagen_hase_silber3_ohne_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit ozeanblauem Längsträger als Nachläufer mit Steuerabteil nach hinten. Front ohne Gummiwulst und Übergang.",
+
+        ["nwagen_kl2_karlsruher_name"] = "Bnrzb 725",
+        ["nwagen_kl2_karlsruher_desc"] = "Mitte der 1970er-Jahre entwickelte das Ausbesserungswerk Karlsruhe für den S-Bahnbetrieb im Ruhrgebiet drei Prototypen für einen Wendezug. Grund dafür waren u.a. Beschwerden der Reisenden, dass bei den langen Strecken im Ruhrgebiet in den Triebwagen kein WC vorhanden war. Als Grundlage dienten die damals in großer Zahl vorhandenen Silberlinge. Farblich passend dazu wurde als Zuglok 141 248 lackiert, die als \"Karlsruher Zug\" im Einsatz waren. 2. Klasse Wagen in Sonderlackierung des Karlsruher Zuges.",
+        ["nwagen_kl1_karlsruher_name"] = "ABnrzb 704",
+        ["nwagen_kl1_karlsruher_desc"] = "Mitte der 1970er-Jahre entwickelte das Ausbesserungswerk Karlsruhe für den S-Bahnbetrieb im Ruhrgebiet drei Prototypen für einen Wendezug. Grund dafür waren u.a. Beschwerden der Reisenden, dass bei den langen Strecken im Ruhrgebiet in den Triebwagen kein WC vorhanden war. Als Grundlage dienten die damals in großer Zahl vorhandenen Silberlinge. Farblich passend dazu wurde als Zuglok 141 248 lackiert, die als \"Karlsruher Zug\" im Einsatz waren. 1./2. Klasse Wagen in Sonderlackierung des Karlsruher Zuges. ",
+
+        ["nwagen_kl2_citybahn1_name"] = "Bnrzb 778.1 \"City-Bahn\"",
+        ["nwagen_kl2_citybahn1_desc"] = "Im Jahr 1984 nahm die erste City-Bahn bei Köln ihren Betrieb auf. Dafür wurde eine neue Lackierung in orange/kieselgrau eingeführt. 2. Klasse Wagen im City-Bahn-Design.",
+        ["nwagen_kl1_citybahn1_name"] = "ABnrzb 772.1 \"City-Bahn\"",
+        ["nwagen_kl1_citybahn1_desc"] = "Im Jahr 1984 nahm die erste City-Bahn bei Köln ihren Betrieb auf. Dafür wurde eine neue Lackierung in orange/kieselgrau eingeführt. 1./2. Klasse Wagen im City-Bahn-Design.",
+        ["nwagen_kl1_citybahn1_bistro_name"] = "ABnrzb 772.1 Bistro \"City-Bahn\"",
+        ["nwagen_kl1_citybahn1_bistro_desc"] = "Im Jahr 1984 nahm die erste City-Bahn bei Köln ihren Betrieb auf. Dafür wurde eine neue Lackierung in orange/kieselgrau eingeführt. 1./2. Klasse Wagen mit Bistro im City-Bahn-Design.",
+
+        ["nwagen_kl2_citybahn2_name"] = "Bnrzb 778.1 \"City-Bahn\"",
+        ["nwagen_kl2_citybahn2_desc"] = "Nach Einführung des orientroten Farbschemas ab 1986 wurden die City-Bahn-Wagen im Design leicht angepaßt. 2. Klasse Wagen im modernisierten City-Bahn-Design.",
+        ["nwagen_kl1_citybahn2_name"] = "ABnrzb 772.1 \"City-Bahn\"",
+        ["nwagen_kl1_citybahn2_desc"] = "Nach Einführung des orientroten Farbschemas ab 1986 wurden die City-Bahn-Wagen im Design leicht angepaßt. 1./2. Klasse Wagen im modernisierten City-Bahn-Design.",
+
+        ["nwagen_karlsruher_silber1_name"] = "BDnf 738 (\"Karlsruher Kopf\")",
+        ["nwagen_karlsruher_silber1_desc"] = "Steuerwagen der n-Wagen-Serie gebaut im AW Karlsruhe, daher \"Karlsruher Kopf\" genannt. Im Gegensatz zu den bisherigen Steuerwagen bot er eine bessere Streckensicht und mehr Platz. Das Gepäckabteil blieb erhalten. Front komplett silber.",
+        ["nwagen_karlsruher1_silber1_name"] = "BDnf 738 (\"Karlsruher Kopf\")",
+        ["nwagen_karlsruher1_silber1_desc"] = "Steuerwagen der n-Wagen-Serie gebaut im AW Karlsruhe, daher \"Karlsruher Kopf\" genannt. Im Gegensatz zu den bisherigen Steuerwagen bot er eine bessere Streckensicht und mehr Platz. Das Gepäckabteil blieb erhalten. Front mit orangenen Kontrastflächen.",
+
+        ["nwagen_karlsruher1_karlsruher_fw_name"] = "BDnf 738 (\"Karlsruher Kopf\") (Steuerabteil voraus)",
+        ["nwagen_karlsruher1_karlsruher_fw_desc"] = "Steuerwagen der n-Wagen-Serie gebaut im AW Karlsruhe, daher \"Karlsruher Kopf\" genannt. Im Gegensatz zu den bisherigen Steuerwagen bot er eine bessere Streckensicht und mehr Platz. Wagen mit Steuerabteil vorne. Farbgebung in Sonderlackierung des Karlsruher Zuges.",
+
+        ["nwagen_karlsruher1_karlsruher_bw_name"] = "BDnf 738 (\"Karlsruher Kopf\") (Steuerabteil hinten)",
+        ["nwagen_karlsruher1_karlsruher_bw_desc"] = "Steuerwagen der n-Wagen-Serie gebaut im AW Karlsruhe, daher \"Karlsruher Kopf\" genannt. Im Gegensatz zu den bisherigen Steuerwagen bot er eine bessere Streckensicht und mehr Platz. Wagen mit Steuerabteil nach hinten. Farbgebung in Sonderlackierung des Karlsruher Zuges.",
+	},
+	de = {
+		["mod_name"] = "n-Wagen",
+		["mod_desc"] = "Als n-Wagen wird eine Gattung von Personenwagen der Deutschen Bundesbahn beziehungsweise der Deutschen Bahn AG bezeichnet. Umgangssprachlich ist die Wagengattung als Silberling bekannt geworden. Diese Bezeichnung verdanken die Nahverkehrswagen der Farbe des ursprünglich unlackierten Wagenkastens aus blankem rostfreiem Stahl mit aufgebürstetem Pfauenaugenmuster. Die Wagen wurden zwischen 1958 und 1980 in rund 5000 Exemplaren gebaut und sind teilweise bis heute bei DB Regio im Einsatz. ",
+
+        ["menu_nwagen"] = "n-Wagen",
+        ["nwagen_kl2_vorserie_name"] = "B4nb (Vorserie)",
+		["nwagen_kl2_vorserie_desc"] = "Vorserienexemplar der n-Wagen, Farbgebung: chromoxidgrün.",
+
+        ["nwagen_kl2_silber1_name"] = "B4nb",
+		["nwagen_kl2_silber1_desc"] = "n-Wagen (\"Silberling\") als 2. Klasse-Wagen und schwarzem Längsträger.",
+        ["nwagen_kl1_silber1_name"] = "AB4nb",
+		["nwagen_kl1_silber1_desc"] = "n-Wagen (\"Silberling\") als 1./2. Klasse-Wagen und schwarzem Längsträger.",
+
+        ["nwagen_kl2_silber2_name"] = "Bnb 720",
+		["nwagen_kl2_silber2_desc"] = "n-Wagen (\"Silberling\") als 2. Klasse-Wagen und schwarzem Längsträger.",
+        ["nwagen_kl1_silber2_name"] = "ABnb 703",
+		["nwagen_kl1_silber2_desc"] = "n-Wagen (\"Silberling\") als 1./2. Klasse-Wagen und schwarzem Längsträger.",
+
+        ["nwagen_kl2_silber3_name"] = "Bnb 720",
+		["nwagen_kl2_silber3_desc"] = "n-Wagen (\"Silberling\") als 2. Klasse-Wagen und ozeanblauem Längsträger.",
+        ["nwagen_kl1_silber3_name"] = "ABnb 703",
+		["nwagen_kl1_silber3_desc"] = "n-Wagen (\"Silberling\") als 1./2. Klasse-Wagen und ozeanblauem Längsträger.",
+
+        ["nwagen_hase_silber1_forward_name"] = "BD4nf (Steuerabteil voraus)",
+        ["nwagen_hase_silber1_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger mit dem Steuerabteil nach vorne und Fahrer.",
+        ["nwagen_hase_silber1_backward_name"] = "BD4nf (Steuerabteil hinten)",
+        ["nwagen_hase_silber1_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger als Nachläufer mit Steuerabteil nach hinten.",
+        ["nwagen_hase_silber1_ohne_forward_name"] = "BD4nf (Steuerabteil voraus)",
+        ["nwagen_hase_silber1_ohne_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger mit dem Steuerabteil nach vorne und Fahrer. Front ohne Gummiwulst und Übergang.",
+        ["nwagen_hase_silber1_ohne_backward_name"] = "BD4nf (Steuerabteil hinten)",
+        ["nwagen_hase_silber1_ohne_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger als Nachläufer mit Steuerabteil nach hinten. Front ohne Gummiwulst und Übergang.",
+
+        ["nwagen_hase_silber2_forward_name"] = "BDnf 738 (Steuerabteil voraus)",
+        ["nwagen_hase_silber2_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger mit dem Steuerabteil nach vorne und Fahrer.",
+        ["nwagen_hase_silber2_backward_name"] = "BDnf 738 (Steuerabteil hinten)",
+        ["nwagen_hase_silber2_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger als Nachläufer mit Steuerabteil nach hinten.",
+        ["nwagen_hase_silber2_ohne_forward_name"] = "BDnf 738 (Steuerabteil voraus)",
+        ["nwagen_hase_silber2_ohne_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger mit dem Steuerabteil nach vorne und Fahrer. Front ohne Gummiwulst und Übergang.",
+        ["nwagen_hase_silber2_ohne_backward_name"] = "BDnf 738 (Steuerabteil hinten)",
+        ["nwagen_hase_silber2_ohne_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit schwarzem Längsträger als Nachläufer mit Steuerabteil nach hinten. Front ohne Gummiwulst und Übergang.",
+
+        ["nwagen_hase_silber3_ohne_forward_name"] = "BDnf 738 (Steuerabteil voraus)",
+        ["nwagen_hase_silber3_ohne_forward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit ozeanblauem Längsträger mit dem Steuerabteil nach vorne und Fahrer. Front ohne Gummiwulst und Übergang.",
+        ["nwagen_hase_silber3_ohne_backward_name"] = "BDnf 738 (Steuerabteil hinten)",
+        ["nwagen_hase_silber3_ohne_backward_desc"] = "n-Wagen (\"Silberling\") mit 2. Klasse und Steuerwagen, sog. \"Hasenkasten\", mit ozeanblauem Längsträger als Nachläufer mit Steuerabteil nach hinten. Front ohne Gummiwulst und Übergang.",
+
+        ["nwagen_kl2_karlsruher_name"] = "Bnrzb 725",
+        ["nwagen_kl2_karlsruher_desc"] = "Mitte der 1970er-Jahre entwickelte das Ausbesserungswerk Karlsruhe für den S-Bahnbetrieb im Ruhrgebiet drei Prototypen für einen Wendezug. Grund dafür waren u.a. Beschwerden der Reisenden, dass bei den langen Strecken im Ruhrgebiet in den Triebwagen kein WC vorhanden war. Als Grundlage dienten die damals in großer Zahl vorhandenen Silberlinge. Farblich passend dazu wurde als Zuglok 141 248 lackiert, die als \"Karlsruher Zug\" im Einsatz waren. 2. Klasse Wagen in Sonderlackierung des Karlsruher Zuges.",
+        ["nwagen_kl1_karlsruher_name"] = "ABnrzb 704",
+        ["nwagen_kl1_karlsruher_desc"] = "Mitte der 1970er-Jahre entwickelte das Ausbesserungswerk Karlsruhe für den S-Bahnbetrieb im Ruhrgebiet drei Prototypen für einen Wendezug. Grund dafür waren u.a. Beschwerden der Reisenden, dass bei den langen Strecken im Ruhrgebiet in den Triebwagen kein WC vorhanden war. Als Grundlage dienten die damals in großer Zahl vorhandenen Silberlinge. Farblich passend dazu wurde als Zuglok 141 248 lackiert, die als \"Karlsruher Zug\" im Einsatz waren. 1./2. Klasse Wagen in Sonderlackierung des Karlsruher Zuges. ",
+
+        ["nwagen_kl2_citybahn1_name"] = "Bnrzb 778.1 \"City-Bahn\"",
+        ["nwagen_kl2_citybahn1_desc"] = "Im Jahr 1984 nahm die erste City-Bahn bei Köln ihren Betrieb auf. Dafür wurde eine neue Lackierung in orange/kieselgrau eingeführt. 2. Klasse Wagen im City-Bahn-Design.",
+        ["nwagen_kl1_citybahn1_name"] = "ABnrzb 772.1 \"City-Bahn\"",
+        ["nwagen_kl1_citybahn1_desc"] = "Im Jahr 1984 nahm die erste City-Bahn bei Köln ihren Betrieb auf. Dafür wurde eine neue Lackierung in orange/kieselgrau eingeführt. 1./2. Klasse Wagen im City-Bahn-Design.",
+        ["nwagen_kl1_citybahn1_bistro_name"] = "ABnrzb 772.1 Bistro \"City-Bahn\"",
+        ["nwagen_kl1_citybahn1_bistro_desc"] = "Im Jahr 1984 nahm die erste City-Bahn bei Köln ihren Betrieb auf. Dafür wurde eine neue Lackierung in orange/kieselgrau eingeführt. 1./2. Klasse Wagen mit Bistro im City-Bahn-Design.",
+
+        ["nwagen_kl2_citybahn2_name"] = "Bnrzb 778.1 \"City-Bahn\"",
+        ["nwagen_kl2_citybahn2_desc"] = "Nach Einführung des orientroten Farbschemas ab 1986 wurden die City-Bahn-Wagen im Design leicht angepaßt. 2. Klasse Wagen im modernisierten City-Bahn-Design.",
+        ["nwagen_kl1_citybahn2_name"] = "ABnrzb 772.1 \"City-Bahn\"",
+        ["nwagen_kl1_citybahn2_desc"] = "Nach Einführung des orientroten Farbschemas ab 1986 wurden die City-Bahn-Wagen im Design leicht angepaßt. 1./2. Klasse Wagen im modernisierten City-Bahn-Design.",
+
+        ["nwagen_karlsruher_silber1_name"] = "BDnf 738 (\"Karlsruher Kopf\")",
+        ["nwagen_karlsruher_silber1_desc"] = "Steuerwagen der n-Wagen-Serie gebaut im AW Karlsruhe, daher \"Karlsruher Kopf\" genannt. Im Gegensatz zu den bisherigen Steuerwagen bot er eine bessere Streckensicht und mehr Platz. Das Gepäckabteil blieb erhalten. Front komplett silber.",
+        ["nwagen_karlsruher1_silber1_name"] = "BDnf 738 (\"Karlsruher Kopf\")",
+        ["nwagen_karlsruher1_silber1_desc"] = "Steuerwagen der n-Wagen-Serie gebaut im AW Karlsruhe, daher \"Karlsruher Kopf\" genannt. Im Gegensatz zu den bisherigen Steuerwagen bot er eine bessere Streckensicht und mehr Platz. Das Gepäckabteil blieb erhalten. Front mit orangenen Kontrastflächen.",
+
+        ["nwagen_karlsruher1_karlsruher_fw_name"] = "BDnf 738 (\"Karlsruher Kopf\") (Steuerabteil voraus)",
+        ["nwagen_karlsruher1_karlsruher_fw_desc"] = "Steuerwagen der n-Wagen-Serie gebaut im AW Karlsruhe, daher \"Karlsruher Kopf\" genannt. Im Gegensatz zu den bisherigen Steuerwagen bot er eine bessere Streckensicht und mehr Platz. Wagen mit Steuerabteil vorne. Farbgebung in Sonderlackierung des Karlsruher Zuges.",
+
+        ["nwagen_karlsruher1_karlsruher_bw_name"] = "BDnf 738 (\"Karlsruher Kopf\") (Steuerabteil hinten)",
+        ["nwagen_karlsruher1_karlsruher_bw_desc"] = "Steuerwagen der n-Wagen-Serie gebaut im AW Karlsruhe, daher \"Karlsruher Kopf\" genannt. Im Gegensatz zu den bisherigen Steuerwagen bot er eine bessere Streckensicht und mehr Platz. Wagen mit Steuerabteil nach hinten. Farbgebung in Sonderlackierung des Karlsruher Zuges.",
+	},
+
+}
+end
