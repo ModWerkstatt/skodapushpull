bcfa4beca6381258b417acb5c2ffcd9c315a7a5c
Florian Thiel <flth@flth.de>
2024-06-13 00:03:03 +0200

    Wagen überarbeitet, MU ferti gestellt

--- res/config/multiple_unit/skodapushpull.lua
+++ res/config/multiple_unit/skodapushpull.lua
@@ -1,12 +1,12 @@
 function data()
 return {
 	vehicles = {
-		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_sw_gedreht.mdl", forward = true },
-		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
-		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
-		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
-		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
 		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_ew.mdl", forward = true },
+		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
+		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
+		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
+		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
+		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_sw.mdl", forward = true },
 	},
 	name = _("skodapushpull_name"),
 	desc = _("skodapushpull_desc"),
