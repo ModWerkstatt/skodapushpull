eb90b6d571c0780c6b37da37bf57d0c4971d07eb
Florian Thiel <flth@flth.de>
2024-06-22 22:59:23 +0200

    gedrehte Versionen und Script für Einzelwagen eingebunden

--- mod.lua
+++ mod.lua
@@ -1,46 +1,46 @@
-﻿function data()
+function data()
 return {
-	info = {
-		minorVersion = 0,
-		severityAdd = "NONE",
-		severityRemove = "WARNING",
-		name = _("mod_name"),
-		description = _("mod_desc"),
-		authors = {
-		    {
-		        name = "ModWerkstatt",
-		        role = "CREATOR",
-		    },
-		},
-		tags = { "europe", "germany", "bahn", "skoday", "dostos", "coach", "nim" },
-        params = {
-	      {
-            key = "show_single_cars",
-            name = _("Show single cars?"),
-            uiType = "CHECKBOX",
-            values = { "No", "Yes", },
-            defaultIndex = 0,
-	      },
+    info = {
+        minorVersion = 0,
+        severityAdd = "NONE",
+        severityRemove = "WARNING",
+        name = _("mod_name"),
+        description = _("mod_desc"),
+        authors = {
+            {
+                name = "ModWerkstatt",
+                role = "CREATOR",
+            },
         },
-	},
+        tags = { "europe", "germany", "bahn", "skoday", "dostos", "coach", "nim" },
+        params = {
+            {
+                key = "show_single_cars",
+                name = _("Show single cars?"),
+                uiType = "CHECKBOX",
+                values = { "No", "Yes", },
+                defaultIndex = 0,
+                tooltip = _("Show only fixed combination or also cars separately?"),
+            },
+        },
+    },
 
-	runFn = function (settings, modParams)
-      local function legacyFilter(fileName, data)
-        if data.metadata.transportVehicle.multipleUnitOnly then
+    runFn = function (settings, modParams)
+    local function SingleCarModifier(fileName, data)
+        if data.metadata and data.metadata.skoda_dosto and data.metadata.transportVehicle and data.metadata.transportVehicle.multipleUnitOnly then
             data.metadata.transportVehicle.multipleUnitOnly = false
         end
-		    return data
-		end
+        return data
+    end
 
-        if modParams[getCurrentModId()] ~= nil then
-			local params = modParams[getCurrentModId()]
-			if params["show_single_cars"] == 1 then
-				addModifier("loadModel", legacyFilter)
-			end
-
-      else
-			addModifier("loadModel", legacyFilter)
-     end
-	end
-	}
+    if modParams[getCurrentModId()] ~= nil then
+    	local params = modParams[getCurrentModId()]
+      	if params["show_single_cars"] == 1 then
+        	addModifier("loadModel", SingleCarModifier)
+        end
+    else
+      	addModifier("loadModel", SingleCarModifier)
+    end
+end
+}
 end
--- strings.lua
+++ strings.lua
@@ -6,17 +6,22 @@ return {
 		["mod_desc"] = "The Škoda reversible train sets are double-decker coaches for locomotive-hauled trains from railway manufacturer Škoda Transportation. In Germany, the carriages are used on the Munich-Nuremberg Express.\nThe carriages were ordered in 2013 and were due to be used from 2017, but the first carriages will not be delivered until 2020. All carriages consist of a control and end carriage and four centre carriages. They are only allowed to run with a Class 102, which is also from Škoda.\nThe carriages have never been particularly successful, so no more were purchased and they are expected to be taken out of service again at the end of 2027.",
 
         ["SkodaDosto_menu"] = "Nuremberg-Munich Express",
-        ["SkodaPushPull_ew_name"] = "End carriage DABpz 790.1",
+        ["SkodaPushPull_ew_name"] = "End carriage DABpz 790.1 [pulled]",
+        ["SkodaPushPull_ew_gedreht_name"] = "End carriage DABpz 790.1 [pushed]",
 		["SkodaPushPull_mw_name"] = "Centre carriage DBpdz 790.0",
-        ["SkodaPushPull_sw_name"] = "Control car DABpbzf 770.0",
-		["skodapushpull_name"] = "Nuremberg-Munich Express pulled",
-        ["skodapushpull_gedreht_name"] = "Nuremberg-Munich Express pushed",
+        ["SkodaPushPull_sw_name"] = "Control car DABpbzf 770.0 [pulled]",
+        ["SkodaPushPull_sw_gedreht_name"] = "Control car DABpbzf 770.0 [pushed]",
+		["skodapushpull_name"] = "Nuremberg-Munich Express [pulled]",
+        ["skodapushpull_gedreht_name"] = "Nuremberg-Munich Express [pushed]",
 		["skodapushpull_desc"] = "The Škoda reversible train sets are double-decker coaches for locomotive-hauled trains from railway manufacturer Škoda Transportation. In Germany, the carriages are used on the Munich-Nuremberg Express.\nThe carriages were ordered in 2013 and were due to be used from 2017, but the first carriages will not be delivered until 2020. All carriages consist of a control and end carriage and four centre carriages. They are only allowed to run with a Class 102, which is also from Škoda.\nThe carriages have never been particularly successful, so no more were purchased and they are expected to be taken out of service again at the end of 2027.",
 
         ["Skoda Nürnberg-München-Express"] = "Škoda Nuremberg-Munich Express",
         ["Statische Assets für die Wagen."] = "Static assets of the Škoda double-decker coaches.",
         ["skoda_nim_assets"] = "Assets of the Škoda double-decker coaches as End car - Center car - Control car.",
         ["Skoda Nürnberg-München-Express Wagen"] = "Skoda Nuremberg-Munich Express cars",
+
+        ["Show single cars?"] = "Show cars separately?",
+        ["Show only fixed combination or also cars separately?"] = "Should only be the fixed combinations are shown in depot or all cars separately?",
 
 	},
 	de = {
@@ -25,17 +30,22 @@ return {
 		["mod_desc"] = "Die Škoda-Wendezug-Garnituren sind Doppelstockwagen für lokomotivbespannte Züge des Eisenbahnherstellers Škoda Transportation. In Deutschland werden die Garnituren auf dem München-Nürnberg-Express eingesetzt.\nDie Wagen wurden 2013 bestellt und sollten ab 2017 eingesetzt werden, die ersten Garnituren liefern aber erst um Jahr 2020. Alle Garnituren bestehen aus je einem Steuer- und Endwagen sowie vier Mittelwagen. Verkehren dürfen sie nur mit der Bespannung einer BR 102, die ebenfalls von Škoda stammt.\nBesonders erfolgreich sind die Wagen nie gewesen, so wurden auch keine weiteren beschafft und zum Jahresende 2027 werden sie voraussichtlich wieder ausser Betrieb gehen.",
 
         ["SkodaDosto_menu"] = "Nürnberg-München-Express",
-        ["SkodaPushPull_ew_name"] = "Endwagen DABpz 790.1",
+        ["SkodaPushPull_ew_name"] = "Endwagen DABpz 790.1 [gezogen]",
+        ["SkodaPushPull_ew_gedreht_name"] = "Endwagen DABpz 790.1 [geschoben]",
 		["SkodaPushPull_mw_name"] = "Mittelwagen DBpdz 790.0",
-        ["SkodaPushPull_sw_name"] = "Steuerwagen DABpbzf 770.0",
-		["skodapushpull_name"] = "Nürnberg-München-Express gezogen",
-        ["skodapushpull_gedreht_name"] = "Nürnberg-München-Express geschoben",
+        ["SkodaPushPull_sw_name"] = "Steuerwagen DABpbzf 770.0 [gezogen]",
+        ["SkodaPushPull_sw_gedreht_name"] = "Steuerwagen DABpbzf 770.0 [geschoben]",
+		["skodapushpull_name"] = "Nürnberg-München-Express [gezogen]",
+        ["skodapushpull_gedreht_name"] = "Nürnberg-München-Express [geschoben]",
 		["skodapushpull_desc"] = "Die Škoda-Wendezug-Garnituren sind Doppelstockwagen für lokomotivbespannte Züge des Eisenbahnherstellers Škoda Transportation. In Deutschland werden die Garnituren auf dem München-Nürnberg-Express eingesetzt.\nDie Wagen wurden 2013 bestellt und sollten ab 2017 eingesetzt werden, die ersten Garnituren liefern aber erst um Jahr 2020. Alle Garnituren bestehen aus je einem Steuer- und Endwagen sowie vier Mittelwagen. Verkehren dürfen sie nur mit der Bespannung einer BR 102, die ebenfalls von Škoda stammt.\nBesonders erfolgreich sind die Wagen nie gewesen, so wurden auch keine weiteren beschafft und zum Jahresende 2027 werden sie voraussichtlich wieder ausser Betrieb gehen.",
 
         ["Skoda Nürnberg-München-Express"] = "Škoda Nürnberg-München-Express",
         ["Statische Assets für die Wagen."] = "Statische Assets für die Škoda Doppelstockwagen.",
         ["skoda_nim_assets"] = "Assets der Škoda Doppelstockwagen als Endwagen - Mittelwagen - Steuerwagen.",
         ["Skoda Nürnberg-München-Express Wagen"] = "Skoda Nürnberg-München-Express Wagen",
+
+        ["Show single cars?"] = "Wagen einzeln anzeigen?",
+        ["Show only fixed combination or also cars separately?"] = "Legt fest, ob nur die festen Kombinationen angezeigt werden im Depot oder alle Wagen auch einzeln.",
 
 	},
 
