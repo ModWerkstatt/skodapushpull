89c70b4a88ee01a1260fbe366485be8d22073a3c
Florian Thiel <flth@flth.de>
2026-06-14 19:16:04 +0200

    Anpassung an Build 40251

---
+++ content/vehicle/waggon/skodapushpull/skodapushpull.mu.lua
@@ -0,0 +1,16 @@
+function data()
+    return {
+	    vehicles = {
+		    { name = "skodadosto_ew.mdl", forward = true },
+		    { name = "skodadosto_mw.mdl", forward = true },
+		    { name = "skodadosto_mw.mdl", forward = true },
+		    { name = "skodadosto_mw.mdl", forward = true },
+		    { name = "skodadosto_mw.mdl", forward = true },
+		    { name = "skodadosto_sw.mdl", forward = true },
+	    },
+	    name = _("skodapushpull_name"),
+	    desc = _("skodapushpull_desc"),
+        groupFileName = "menu_skodadosto.mdl",
+        filterTags = {"default"},
+    }
+end
